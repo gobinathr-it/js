5bf8afcebf632b425bfc9fb90c1e3525ed700e38
gobinathrit-tech <gobinathr2024it@jkkn.ac.in>
2026-03-12 11:18:44 +0530

    added js practice files

---
+++ demo.js
@@ -0,0 +1,127 @@
+/*function favourite()
+{
+    var factor = "Burush banner"
+    var fplayer = "Dhoni"
+    var fmovie = "Avengers"
+
+    console.log("favourite actor is " + factor +
+         "favourite player is " + fplayer +
+          "favourite movie is " + fmovie)
+}
+favourite()
+
+function add(a,b)
+{
+    console.log(a+b)
+}
+add()*/  
+
+// if else in js
+/*
+rain=true
+if(rain)
+{
+    console.log("take an umberlla")
+}
+else{   
+    console.log("enjoy the sunshine")
+}
+
+var homework = false
+if(homework)
+{
+    console.log("do your homework")
+}
+else{
+    console.log("play with your friends")
+}
+var cookies=false
+if(cookies)
+{
+    console.log("Would you like to eat the cookies")
+}
+else{
+    console.log("don't eat the cookies")
+}
+var brush = false
+if(brush)
+{
+    console.log("your teeth are clean")
+}
+else{
+    console.log("go and brush your teeth")
+}*
+
+var birthday = false
+if(birthday)
+{
+    console.log("happy birthday!")
+}
+else{
+    console.log("have a great day!")
+}
+
+console.log(true && false && true)
+console.log(false || true || false) //1.05 finsihed
+
+/*var kalai = false
+if(kalai){
+    console.log("Avg student")
+}
+else{
+    console.log("class toper")
+}
+console.log(!false)
+var session = "summer"
+if(session == "spring"){
+    console.log("enjoy the blooming flowers")
+}
+if(session == "summer"){
+    console.log("have fun in the sun")
+}
+if(session == "autumn"){
+    console.log("admire the colorful leaves")
+
+
+else{
+}
+var score = 73
+if(score<50)
+{
+    console.log("need improvement")
+}
+else if(score>50 && score<=70)
+{
+    console.log("Good & Super")
+}
+else if(score>70 ){
+    console.log("Excellent")
+}
+/*
+var num = 8
+if(num % 2 === 0)
+{
+    console.log("the number is even")
+}
+else{
+    console.log("odd number")
+}
+
+var vowel = "e"
+if(vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u" ){
+    console.log("it is a vowel")
+}
+else{
+    console.log("its is constent")
+}
+for (let i = 1; i <= 10; i=i+1) {
+    if(i % 2 == 0){
+            console.log("even nums are")
+            console.log(i)
+    }
+}
+
+// table
+for (let i = 1; i <= 10; i=i+1) {
+    console.log(i+"x2="+ i*2)
+}*/
